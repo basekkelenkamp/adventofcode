27620f7c8bd5b7190cd16e6fafcbf9c0412e7ac4
Bas <57452503+basekkelenkamp@users.noreply.github.com>
2024-12-09 18:00:28 +0100

    Update day7.py

--- 2024/day7.py
+++ 2024/day7.py
@@ -9,6 +9,8 @@ def evaluate_left_to_right(nums, operators):
             result += nums[i + 1]
         elif op == "*":
             result *= nums[i + 1]
+        elif op == "||":
+            result = int(str(result) + str(nums[i + 1]))
     return result
 
 count = 0
@@ -16,12 +18,12 @@ for line in lines:
     p1, p2 = line.split(":")
     result = int(p1)
     nums = [int(x) for x in p2.split(" ")[1:] if x.isdigit()]
-    if len(nums) < 2 and result in [nums[0] * nums[1], nums[0] + nums[1]]:
+    if len(nums) < 2 and result in [nums[0] * nums[1], nums[0] + nums[1]] or result == int(str(nums[0]) + str(nums[1])):
         count += result
         continue
 
 
-    op_combinations = product(["+", "*"], repeat=len(nums) - 1)
+    op_combinations = product(["+", "*", "||"], repeat=len(nums) - 1)
     for op_combination in op_combinations:
         res = evaluate_left_to_right(nums, op_combination)
         if res == result:
