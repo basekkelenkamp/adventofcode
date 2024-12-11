b33ea721e94a0a0cbcf293ff71c2499f7759a2ea
Bas  Ekkelenkamp <b.ekkelenkamp@kotug.com>
2024-12-11 13:10:17 +0100

    day8 still wip

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,40 +1,41 @@
-def check_around(lines, char, h, w):
-    directions = [
-        (1, 0),  # Down
-        (-1, 0),  # Up
-        (0, 1),  # Right
-        (0, -1),  # Left
-        (1, 1),  # Down-Right
-        (-1, -1),  # Up-Left
-        (1, -1),  # Down-Left
-        (-1, 1)  # Up-Right
-    ]
+from itertools import combinations
+from collections import defaultdict
 
-    max_depth = max(len(lines), len(lines[0]))
-    depth = 1
-    while depth <= max_depth:
-        for dh, dw in directions:
-            nh, nw = h + depth * dh, w + depth * dw
-            if 0 <= nh < len(lines) and 0 <= nw < len(lines[0]) and lines[nh][nw] == char:
-                return nh, nw
-        depth += 1
+def add_tuple(t1, t2):
+    return t1[0] + t2[0], t1[1] + t2[1]
 
-    return False
-
+def subtract_tuple(t1, t2):
+    return t1[0] - t2[0], t1[1] - t2[1]
 
 def main():
     file = open("data/8.txt", "r")
     lines = [line.strip() for line in file.readlines()]
-    anti_nodes = []
+    char_pos = defaultdict(list)
+    board_size = (len(lines) - 1, len(lines[0]) - 1)
     for h, line in enumerate(lines):
         for w, char in enumerate(line):
             if char == ".":
                 continue
-            print(f"Checking: {char}, pos: {h},{w}")
-            pos = check_around(lines, char, h, w)
-            if pos:
-                print(f"found '{char}': {pos}")
+            char_pos[char].append((h, w))
 
+    anti_nodes = []
+    combs = defaultdict(list)
+    for char, positions in char_pos.items():
+        if len(positions) > 1:
+            for comb in combinations(positions, 2):
+                combs[char].append(list(comb))
 
+                diff = (comb[1][0] - comb[0][0], comb[1][1] - comb[0][1])
+                print(f"{comb} -> diff: {diff}")
+                add = add_tuple(comb[0], diff)
+                sub = subtract_tuple(comb[1], diff)
+                print(f"add: {add}, sub: {sub}")
+                if all([add[0] >= 0, add[0] < board_size[0], add[1] >= 0, add[1] < board_size[1]]):
+                    anti_nodes.append(add)
+                if all([sub[0] >= 0, sub[0] < board_size[0], sub[1] >= 0, sub[1] < board_size[1]]):
+                    anti_nodes.append(sub)
+
+    print(len(anti_nodes))
+    print(len(set(anti_nodes)))
 if __name__ == "__main__":
     main()
